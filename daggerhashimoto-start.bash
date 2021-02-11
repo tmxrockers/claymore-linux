0daabb19576a662f81133473222abcd4d44db514
Arun <arundeepan1000@gmail.com>
2021-02-11 15:03:16 +0530

    initial commit

---
+++ daggerhashimoto-start.bash
@@ -0,0 +1,1 @@
+./ethdcrminer64 -epool stratum+tcp://daggerhashimoto.usa.nicehash.com:3353 -ewal 3HZdKrNHePza7pg1rM7sYgtBhxueA53FVP.ARUN-daggerhashimoto -epsw x -dbg -1 -retrydelay 1 -ftime 55 -tt 79 -ttli 77 -tstop 89 -tstart 85 -fanmin 30 -r 0 -esm 3 -estale 0 -erate 1 -allpools 1
